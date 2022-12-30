19656e2631fe0992da601a9058c0efc90b037eea
sunilchau <sunil.chaudhari7@gmail.com>
2022-12-30 22:59:09 +0530

    Second Commit

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def print_hello(msg):
+    print(msg)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_hello("Happy New Year 2020!!!!!")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
