d3ad69db209866c0f2aa5077915fd2cab5ff7a3b
sunilchau <sunil.chaudhari7@gmail.com>
2022-12-31 09:24:40 +0530

    git remote first commit

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+def func_init(msg):
+    print(msg)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -14,5 +15,6 @@ def print_hello(msg):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hello("Happy New Year 2020!!!!!")
+    func_init("remote git push")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
